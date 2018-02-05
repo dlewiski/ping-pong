fb2703059bc31a3b809bc33cd52da4394af4f2fd
David Lewis and Kaila Goff <kaila@>
2018-02-05 11:38:34 -0800

    add methods for checking and replacing numbers divisible by 3 and 5

---
+++ lib/ping_pong.rb
@@ -0,0 +1,25 @@
+def ping_pong(number)
+    number_array = []
+    counter = 0
+    while counter < number
+      counter += 1
+      number_array.push(counter)
+  end
+  number_array
+end
+
+def three_check(number_array)
+  new_array = []
+  number_array.each do |number_element|
+    if (number_element % 3 == 0)
+      number_element = "ping"
+      new_array.push(number_element)
+    elsif (number_element % 5 == 0)
+      number_element = "pong"
+      new_array.push(number_element)  
+    else
+      new_array.push(number_element)
+    end
+  end
+  new_array
+end
