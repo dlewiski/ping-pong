fa5ceab0387d0f6ee6ecda3a7d904f22bda9f252
David Lewis and Kaila Goff <kaila@>
2018-02-05 11:50:09 -0800

    add interger class and self

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -1,14 +1,14 @@
 require('rspec')
 require('ping_pong')
 
-# describe('#ping_pong') do
+# describe('#pingpong') do
 #   it("takes number an returns an array of all consecutive intergers of that number") do
 #     expect(ping_pong(5)).to(eq([1, 2, 3, 4, 5]))
 #   end
 # end
 
-describe('#three_check') do
-  it("takes an array of numbers, grabs every number divisible by three and returns 'ping'") do
-    expect(three_check([2, 3, 5, 6])).to(eq([2, "ping", "pong", "ping"]))
-  end
-end
+# describe('#three_check') do
+#   it("takes an array of numbers, grabs every number divisible by three and returns 'ping'") do
+#     expect(three_check([2, 3, 5, 6])).to(eq([2, "ping", "pong", "ping"]))
+#   end
+# end
